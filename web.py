f91711f93acbb04ad487a14b1e0d6cdcf37817b2
ayan.idris <ayanidris@gmail.com>
2025-07-08 01:21:48 +0530

    Create a dynamic web app

--- web.py
+++ web.py
@@ -1,13 +1,26 @@
 import streamlit as st
+from streamlit import checkbox
+
 import function
 
 todos=function.get_todos()
+
+def add_todo():
+    todo1= st.session_state["new_todo"] + "\n"
+    todos.append(todo1)
+    function.write_todos(todos)
 
 st.title("My todo app")
 st.subheader("This is my todo")
 st.write("This app will increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    Checkbox=st.checkbox(todo,key=todo)
+    if Checkbox:
+        todos.pop(index)
+        function.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_return()
 
-st.text_input(label="",placeholder="Add new todo...")
+st.text_input(label="",placeholder="Add new todo...",
+              on_change=add_todo(),key='new_todo')
